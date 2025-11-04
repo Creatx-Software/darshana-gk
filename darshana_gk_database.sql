cd886d70657d012f0bf06054bc0dd99ed270ce00
iamteki <tekiyagaming@gmail.com>
2025-11-05 00:04:19 +0530

    Fix: Clean database export without warnings

--- darshana_gk_database.sql
+++ darshana_gk_database.sql
@@ -1,4 +1,3 @@
-mysqldump: [Warning] Using a password on the command line interface can be insecure.
 -- MySQL dump 10.13  Distrib 8.0.43, for Linux (x86_64)
 --
 -- Host: localhost    Database: darshana_gk
@@ -1822,4 +1821,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-04 23:57:27
+-- Dump completed on 2025-11-05  0:04:01
