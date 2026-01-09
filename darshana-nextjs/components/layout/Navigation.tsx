97f4d98d815a13e9b830d94eb0b7c40344fd2050
iamteki <tekiyagaming@gmail.com>
2026-01-10 02:47:07 +0530

    added custom design icon to header

--- darshana-nextjs/components/layout/Navigation.tsx
+++ darshana-nextjs/components/layout/Navigation.tsx
@@ -43,9 +43,26 @@ export default function Navigation() {
           <li><Link href="/articles" className="nav-link" onClick={closeMenu}>ARTICLES</Link></li>
           <li><Link href="/#about" className="nav-link" onClick={closeMenu}>ABOUT</Link></li>
           <li><Link href="/#contact" className="nav-link" onClick={closeMenu}>CONTACT</Link></li>
-          <li className="nav-cta">
-            <Link href="/custom-design" className="nav-link-button" onClick={closeMenu}>
-              CUSTOM DESIGN
+          <li className="nav-cta-floating">
+            <Link href="/custom-design" className="nav-floating-button" onClick={closeMenu}>
+              <div className="nav-floating-icon">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+              <span className="nav-floating-text">Custom Design</span>
             </Link>
           </li>
         </ul>
